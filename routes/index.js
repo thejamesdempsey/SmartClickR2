23c648aa4e0ff789a2d04050db022c6a62db4f91
Daniel To <danielto0252@gmail.com>
2012-12-21 16:40:38 -0500

    Make auto login work

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
-
-/*
- * GET home page.
- */
+var AM = require('./modules/account-manager');
 
 // GET /index //
-exports.index = function(request, response){
-	response.render('index.jade', { title: 'SmartClickR | Welcome' });
+exports.index = function(request, response) {
+	console.log('User cookie', request.cookies.email);
+	console.log('Pass cookie', request.cookies.pass);
+	if (request.cookies.email == undefined || request.cookies.pass == undefined) {
+		response.render('index.jade', { title: 'SmartClickR | Welcome' });
+	} else {
+		AM.autoLogin(request.cookies.email, request.cookies.pass, function(err, results) {
+			if (results != null) {
+				request.session.user = results;
+				response.redirect('/user/' + results[0].User_ID);
+			} else {
+				response.redirect('/login');
+			}
+		})
+	}
+	
 };
 
 
