9951fdf7007b699fb6f039051daf263c8478913e
thejamesdempsey <hello@thejamesdempsey.com>
2013-01-22 22:03:22 -0500

    random greeting on userpage

---
+++ public/javascript/randomGreeting.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	getQuote();
+});
+
+
+
+function getQuote(){
+	var quote = new Array();
+		quote[0] = "Did you get a haircut? It looks great";
+		quote[1] = "<a href='http://www.youtube.com/watch?v=5_sfnQDr1-o'>Baby monkeys gotta have their fun too!</a>";
+		quote[2] = "The only thing we have to fear is fear itself";
+		quote[3] = "G'day Mate!";
+		quote[4] = "Falling doesnt make you a failure, staying down does.";
+		quote[5] = "Great shirt! Where'd you get it?";
+		quote[6] = "Livin' la vida, SmartClickR style";
+		quote[7] = "Keep on <a href='http://www.youtube.com/watch?v=nDOPNLzEFH0'>poppin' and lockin</a> my friend";
+		quote[8] = "How do you stay in such good shape? <a href='http://www.youtube.com/watch?v=BE1Z-nI4CMI&feature=related'>Jazzercise!</a>"
+		quote[9] = "Its nice having you around :)";
+		quote[10] = "<a href='http://www.youtube.com/watch?v=2Qd_IsxgAf8'>Thundercats!!</a>";
+		quote[11] = "That's no fish. <a href='http://www.youtube.com/watch?v=IhJQp-q1Y1s'>This is a fish</a>"
+	
+	var randomChoice = Math.floor(Math.random() * quote.length);
+	$('#greeting').append(quote[randomChoice]);
+}
+
+
+ 
