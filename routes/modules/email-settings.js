a5717d4b14b641593a41c1595bfdd15206cf9a48
thejamesdempsey <hello@thejamesdempsey.com>
2013-03-14 13:37:15 -0400

    ignor

--- routes/modules/email-settings.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	host		: 'smtp.gmail.com',
-	sender		: 'hellosmartclickr@gmail.com',
-	password 	: 'iamapassword',
-	ssl			: true
-}
--- routes/modules/my-info-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-	user : 'smartclickr',
-	pass : 'Morph1_sc!',
-	host : 'mysql.morphatic.com',
-	db	 : 'smartclickr'
-
-}
