385968da7f2e10cdd8dffbd0f73681d196a326a1
thejamesdempsey <hello@thejamesdempsey.com>
2013-04-17 13:30:39 -0400

    Adding content to the guide

--- public/javascript/sticky-nav.js
+++ public/javascript/sticky-nav.js
@@ -1,63 +1,23 @@
-$(document).ready(function() {
- 
-        //Calculate the height of <header>
-        //Use outerHeight() instead of height() if have padding
-        var aboveHeight = $('#guide-hero').outerHeight();
- 
-	//when scroll
-        $(window).scroll(function(){
- 
-	        //if scrolled down more than the header’s height
-                if ($(window).scrollTop() > aboveHeight){
- 
-	        // if yes, add “fixed” class to the <nav>
-	        // add padding top to the #content 
-                //(value is same as the height of the nav)
-                $('#nav-container').addClass('fixed', 2000).css('top','0').css('z-index', '20').css('box-shadow', '0 2px 5px #b4b4b4');
- 				$('#page-top').removeClass('navbar-fixed-top', 2000);
-				$('#back-top').css('display', 'block');
-				
-				
-				
-				} else {
- 
-	        // when scroll up or less than aboveHeight,
-            //remove the “fixed” class, and the padding-top
-                $('#nav-container').removeClass('fixed', 2000).css('top','80px').css('box-shadow', '0px');
- 				$('#page-top').addClass('navbar-fixed-top');
-				$('#back-top').css('display', 'none');
-				
-				
+$(function() {
 
-               }
+    var $sidebar   = $("#nav-container"), 
+        $window    = $(window),
+        offset     = $sidebar.offset(),
+        topPadding = 15;
 
-				if($(window).width() < '768'){
-					$('#nav-container').removeClass('fixed').next()
-	                .css('padding-top','0');
-					$('#mobile-built').css('padding-top', '34px');
-					$('.banded').css('padding-top', '34px');
-					$('#back-top').css('display', 'none');
-				}
-        });
+    $window.scroll(function() {
+        if ($window.scrollTop() > offset.top) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 0
+            });
+        }
+    });
 
-		var sections = $("section.sub-wrap");
-		var navigation_links = $("#guide-nav li a");
-
-		sections.waypoint({
-			handler: function(event, direction) {
-
-				var active_section;
-				active_section = $(this);
-				if (direction == "up") active_section = active_section.prev();
-
-				var active_link = $('#guide-nav a[href="#' + active_section.attr("id") + '"]');
-				navigation_links.removeClass("active");
-				active_link.addClass("active");
-
-			},
-			offset: '25%'
-		})
-
+	var navigation_links = $("#guide-nav li a");
 		navigation_links.click( function(event) {
 
 			$.scrollTo(
@@ -68,4 +28,6 @@ $(document).ready(function() {
 				}
 			);						
 		});
+
+    
 });
